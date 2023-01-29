263d698ab4a08ae46cef7ca09c2ad1c3bfc10ccd
Joseph Neil Fontanilla <josephfontanilla2@gmail.com>
2023-01-29 18:20:05 -0500

    small refactor

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 const Automerge = require('@automerge/automerge');
 
-let doc1 = Automerge.init();
-let doc2 = Automerge.init();
+function counterPerformance() {
+	let doc1 = Automerge.init();
+	let doc2 = Automerge.init();
+	
+	doc1 = Automerge.change(doc1, (doc) => {
+		// The counter is initialized to 0 by default. You can pass a number to the
+		// Automerge.Counter constructor if you want a different initial value.
+		doc.counter = new Automerge.Counter();
+	});
+	
+	doc2 = Automerge.change(doc2, (doc) => {
+		doc.counter = new Automerge.Counter();
+	});
+	
+	doc1 = Automerge.change(doc1, (doc) => {
+		doc.counter.decrement(10);	// Decrement 10 to counter value
+		doc.counter.increment(1); 	//Add 1 to counter
+	});
+	doc2 = Automerge.change(doc2, (doc) => {
+		doc.counter.increment(1); 	//Increment 1 to counter
+		doc.counter.decrement(10); // Subtract 10 from counter value
+		doc.counter.increment(1); 	// Add 1 to counter value
+	});
+	
+	var startTime = performance.now();
+	let finalDoc = Automerge.merge(doc1, doc2);
+	var endTime = performance.now();
 
-doc1 = Automerge.change(doc1, (doc) => {
-	// The counter is initialized to 0 by default. You can pass a number to the
-	// Automerge.Counter constructor if you want a different initial value.
-	doc.buttonClicks = new Automerge.Counter();
-});
+	console.log(`Final counter value: ${finalDoc.counter}`);
 
-doc2 = Automerge.change(doc2, (doc) => {
-	// The counter is initialized to 0 by default. You can pass a number to the
-	// Automerge.Counter constructor if you want a different initial value.
-	doc.buttonClicks = new Automerge.Counter();
-});
+	return endTime - startTime
+}
 
-doc1 = Automerge.change(doc1, (doc) => {
-	doc.buttonClicks.decrement(10); // Decrement 10 to counter value
-	doc.buttonClicks.increment(1); //Add 1 to counter
-});
-doc2 = Automerge.change(doc2, (doc) => {
-	doc.buttonClicks.increment(1); //Increment 1 to counter
-	doc.buttonClicks.decrement(10); // Subtract 10 from counter value
-	doc.buttonClicks.increment(1); // Add 1 to counter value
-});
-
-var startTime = performance.now();
-let finalDoc = Automerge.merge(doc1, doc2);
-var endTime = performance.now();
-
-console.log('final doc');
-console.log(finalDoc);
-
-console.log(`Time to merge: ${endTime - startTime} milliseconds`);
+console.log(`Time to merge: ${counterPerformance()} milliseconds`);
